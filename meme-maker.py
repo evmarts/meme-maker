8782e553f6156246572216d912664bef510607dd
Evan Martin <evan.martin@shaw.ca>
2017-07-31 01:11:50 -0700

    added user input argument

--- meme-maker.py
+++ meme-maker.py
@@ -4,8 +4,15 @@ from PIL import Image
 from PIL import ImageDraw
 import textwrap
 
+def read_txt_file(file_path):
+	print "Reading text file..."
+	with open(file_path, 'r') as txt_file:
+		data = txt_file.read().replace('\n', '')
+		print "Text read: " + str(data)
+	return data
 
 def paste_pic(im_path,img):
+	print "Pasting image..."
 	pic = Image.open(im_path)
 	pic = pic.resize((900,900))
 	box = (180,360)
@@ -16,21 +23,22 @@ def place_text(text, font, margin, offset, img):
 	return 0
 
 length = width = 1080
-text = "When you blah blah blah blah blah blah blah blah"
+file_path = raw_input("text file to use: ")
+text = read_txt_file(file_path)
 font = ImageFont.truetype("/Library/Fonts/HelveticaNeue Light.ttf",60)
 
 # im = Image.open("img1.jpg")
 # im.resize((100,100))
-img=Image.new("RGBA", (length,width),(255,255,255))
+img=Image.new("RGB", (length,width),(255,255,255))
 draw = ImageDraw.Draw(img)
 margin = 40
 offset = 40
 for line in textwrap.wrap(text, width=36):
     draw.text((margin, offset), line, font=font, fill="black")
     offset += font.getsize(line)[1]
-paste_pic("img1.jpg",img)
+paste_pic("sample_img.jpg",img)
 draw = ImageDraw.Draw(img)
 
-img.save("test.png")
+img.save("meme.jpg")
 
 
