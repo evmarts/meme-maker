77df96ab85426361e79024a839f7276a7d87054b
Evan Martin <evan.martin@shaw.ca>
2017-08-01 11:18:05 -0700

    added calculate_text_box_height

--- meme-maker.py
+++ meme-maker.py
@@ -7,15 +7,33 @@ import textwrap
 
 # PIL.ImageDraw.Draw.polygon(xy, fill=None, outline=None)
 
+def calculate_text_box_height(text):
+	## adding one more line adds 72px to the text-box area
+	## want 57px between text and pic
+	chars_per_line = float(36)
+	line_height = 72
+	number_of_lines = int(math.ceil(len(text)/chars_per_line))
+	text_box_height = 96 + number_of_lines*line_height
+	print "text-box height: " + str(text_box_height)
+	return text_box_height
+
+def calculate_image_box(pic):
+	pic_size = pic.getbbox()
+	print "pic size: " + str(pic_size)
+
+	return 0
+
 def delegate_area(text, pic):
-	return null
+	text_box_height = calculate_text_box_height(text)
+	image_box = calculate_image_box(pic)
+
+	return 0
 
 def resize_img(image, new_width):
 	bbox = image.getbbox()
 	width  = bbox[2]
 	length = bbox[3]
 	aspect_ratio = float(width)/float(length)
-	print bbox
 	print "length: " + str(length)
 	print "width:  " + str(width)
 	print "aspect ratio: " + str(aspect_ratio) + ":1"
@@ -38,7 +56,7 @@ def read_txt_file(file_path):
 
 def paste_pic(pic,img):
 	pic = resize_img(pic, 700)
-	box = (180,360)
+	box = (40,240)
 	img.paste(pic, box=box, mask=None)
 
 def place_text(img,text, draw):
@@ -47,8 +65,6 @@ def place_text(img,text, draw):
 	font = ImageFont.truetype("HelveticaNeue Light.ttf",60)
 
 	chars_per_line = float(36)
-	number_of_lines = int(math.ceil(len(text)/chars_per_line))
-	print number_of_lines
 
 	for line in textwrap.wrap(text, width=chars_per_line):
 		draw.text((margin, offset), line, font=font, fill="black")
@@ -61,32 +77,20 @@ def main():
 	image_path = "sample_img.jpg"	## TODO: remove line
 	length = width = 1080 
 
-	background = Image.new("RGB", (length,width),(255,255,255))
 	text = read_txt_file(file_path)
 	pic = Image.open(image_path)
+
+	delegate_area(text, pic)
+
+	background = Image.new("RGB", (length,width),(255,255,255))
+
 	draw = ImageDraw.Draw(background)
 	place_text(background, text, draw)
 	paste_pic(pic,background)
+
+	text_box = [(0,0),(1079,240)]
+	draw.rectangle(text_box,fill=None, outline = 128)
+
 	background.save("meme.jpg")
 
 main()
-
-# length = width = 1080
-# file_path = raw_input("text file to use: ")
-# text = read_txt_file("sample_text.txt")
-# font = ImageFont.truetype("HelveticaNeue Light.ttf",60)
-
-# # im.resize((100,100))
-# img=Image.new("RGB", (length,width),(255,255,255))
-# draw = ImageDraw.Draw(img)
-# margin = 40
-# offset = 40
-# for line in textwrap.wrap(text, width=36):
-#     draw.text((margin, offset), line, font=font, fill="black")
-#     offset += font.getsize(line)[1]
-
-# image_path = raw_input("image to use: ")
-# paste_pic("sample_img.jpg",img)
-# draw = ImageDraw.Draw(img)
-
-# img.save("meme.jpg")
